13b2a5d9bbcb6ea2747373ea92b41d04a6884920
Anirban Sinha <107923453+AnirbanSinha27@users.noreply.github.com>
2024-05-18 18:45:49 +0530

    updated main

--- src/components/canvas/index.js
+++
@@ -1,6 +0,0 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import ComputersCanvas from "./ComputersCanvas";
-import StarsCanvas from "./Stars";
-
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
